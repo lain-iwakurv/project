be722a828d27f9cc5eb204ed28d249f65cafb2a8
Altynai <livakurv@bk.ru>
2024-10-28 14:46:29 +0500

    add object personalMovieDB

--- js/script.js
+++ js/script.js
@@ -18,4 +18,14 @@
 //     }
 
 let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
+let personalMovieDB = {
+    count: numberOfFilms, 
+    movies: {
 
+    },
+    actors: {
+
+    },
+    genres: [ ], 
+    privat: false
+}
