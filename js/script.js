04962810ccc1dd2ba40abca0d7a5ebd6b357376b
Altynai <livakurv@bk.ru>
2024-11-03 22:16:53 +0500

    add condition

--- js/script.js
+++ js/script.js
@@ -37,7 +37,20 @@ for (let i = 0; i < 2; i++) {
     else {
         console.log("error");
         i--;
-    }
-    
+    }   
 }
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+}
+else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+}
+else if (personalMovieDB.count > 30) {
+    console.log("Вы киноман");
+}
+else {
+    console.log("Произошла ошибка");
+}
+
 console.log(personalMovieDB);
